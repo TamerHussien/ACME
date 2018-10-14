0f4c3b3be2819a4f2538c552eddb1725b40a4331
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-10-14 10:17:40 +0200

    add detail and edit routes in product detail page

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { IProduct } from './product';
 import { ProductService } from './product.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
     templateUrl: './product-detail.component.html'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit{
     pageTitle: string = 'Product Detail';
     product: IProduct;
     errorMessage: string;
 
-    constructor(private productService: ProductService) { }
+    constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            let id = +params['id'];
+            this.getProduct(id);
+        });
+    }
     getProduct(id: number) {
         this.productService.getProduct(id).subscribe(
             product => this.product = product,
