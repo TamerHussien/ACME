f20e0010af06b4adc603c04abf093c0221204929
Tamer Hussein <tamer.hussein@thoughtdesign.com.au>
2018-12-01 09:40:23 +0200

    Routing Styling And Animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { AuthService } from './user/auth.service';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError, NavigationCancel } from '@angular/router';
 
 @Component({
     selector: 'pm-app',
@@ -9,9 +9,21 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
     pageTitle: string = 'Acme Product Management';
+    loading: boolean = true;
+    constructor(private authService: AuthService, private router: Router) {
+        router.events.subscribe((routerEvent: Event) => {
+            this.checkRouteEvent(routerEvent);
+        });
+    }
 
-    constructor(private authService: AuthService, private router: Router) { }
-
+    checkRouteEvent(RouterEvent: Event) {
+        if (RouterEvent instanceof NavigationStart) {
+            this.loading = true;
+        }
+        if (RouterEvent instanceof NavigationEnd || RouterEvent instanceof NavigationCancel || RouterEvent instanceof NavigationError) {
+            this.loading = false;
+        }
+    }
     logOut(): void {
         this.authService.logout();
         this.router.navigateByUrl('/welcome');
